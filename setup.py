aeb8d660e6a6ad1dbffda84b0526c06ad5b10895
David Cook <divergentdave@gmail.com>
2016-12-07 20:23:43 -0600

    Put long description directly in setup.py
    This gets rid of the cutesy open('README.md') trick, which doesn't work
    when running setup.py from inside tox.

--- setup.py
+++ setup.py
@@ -6,7 +6,25 @@ setup(
 	description=' A general purpose PDF text-layer redaction tool for Python 2/3.',
 	author='Joshua Tauberer',
 	author_email='jt@occams.info',
-	long_description=open('README.md', 'r').read(),
+	long_description='''
+	A general-purpose PDF text-layer redaction tool, in pure Python, by Joshua Tauberer and Antoine McGrath.
+
+	pdf-redactor uses pdfrw under the hood to parse and write out the PDF.
+
+	This Python module is a general tool to help you automatically redact text from PDFs. The tool operates on:
+
+	* the text layer of the document's pages (content stream text)
+	* the Document Information Dictionary, a.k.a. the PDF metadata like Title and Author
+	* embedded XMP metadata, if present
+
+	Graphical elements, images, and other embedded resources are not touched.
+
+	You can:
+
+	* Use regular expressions to perform text substitution on the text layer (e.g. replace social security numbers with "XXX-XX-XXXX").
+	* Rewrite, remove, or add new metadata fields on a field-by-field basis (e.g. wipe out all metadata except for certain fields).
+	* Rewrite, remove, or add XML metadata using functions that operate on the parsed XMP DOM (e.g. wipe out XMP metadata).
+	''',
 	url='https://github.com/JoshData/pdf-redactor',
 	py_modules=['pdf_redactor'],
 	classifiers=[
