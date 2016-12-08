5f4f56603ea518ed3f91d02c741b76315bd0ec02
David Cook <divergentdave@gmail.com>
2016-12-07 20:11:37 -0600

    Add metadata test

--- tests/test_redactor.py
+++ tests/test_redactor.py
@@ -1,6 +1,7 @@
 import os
 import pkg_resources
 import re
+import subprocess
 import tempfile
 import textract
 import unittest
@@ -35,6 +36,32 @@ class RedactorTest(unittest.TestCase):
 
 				text = textract.process(redacted_path)
 				self.assertIn(b"Here are some fake SSNs\n\nXXX-XX-XXXX\n--\n\nXXX-XX-XXXX XXX-XX-XXXX\n\nAnd some more with common OCR character substitutions:\nXXX-XX-XXXX XXX-XX-XXXX XXX-XX-XXXX XXX-XX-XXXX XXX-XX-XXXX", text)
+		finally:
+			redacted_file.close()
+			os.unlink(redacted_path)
+
+	def test_metadata(self):
+		redacted_fd, redacted_path = tempfile.mkstemp(".pdf")
+		redacted_file = os.fdopen(redacted_fd, "wb")
+		try:
+			with open(FIXTURE_PATH, "rb") as f:
+				options = pdf_redactor.RedactorOptions()
+				options.input_stream = f
+				options.output_stream = redacted_file
+				options.metadata_filters = {
+					"Title": [lambda value: value.replace("test", "sentinel")],
+					"Subject": [lambda value: value[::-1]],
+					"DEFAULT": [lambda value: None],
+				}
+
+				pdf_redactor.redactor(options)
+				redacted_file.close()
+
+				metadata = subprocess.check_output(["pdfinfo", redacted_path])
+				self.assertIn(b"this is a sentinel", metadata)
+				self.assertIn(b"FDP a si", metadata)
+				self.assertNotIn(b"CreationDate", metadata)
+				self.assertNotIn(b"LibreOffice", metadata)
 		finally:
 			redacted_file.close()
 			os.unlink(redacted_path)
